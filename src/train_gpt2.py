957aabda5a9690719464b90bfff08747521b48b1
Ting <yt0701@gmail.com>
2025-10-15 23:33:14 -0700

    Ting : Add a pynb to show the gelu

--- src/train_gpt2.py
+++ src/train_gpt2.py
@@ -8,8 +8,8 @@ class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
-        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
 
     def forward(self, x):
         x = self.c_fc(x)
